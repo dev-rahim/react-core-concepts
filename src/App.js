14456d1cf24f2b5c742afc0a859298b0c43dc1f9
dev-rahim <rahimuddin5915@gmail.com>
2022-10-11 23:26:27 +0600

    add component data from an array of object and it's works dinamically

--- src/App.js
+++ src/App.js
@@ -2,30 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const employesName = [
-    'Rahim Uddin',
-    'Nabila',
-    'Emon',
-    'Nobita',
-    'Sohag',
-    'Arafat',
+
+  const employesData = [
+    { name: 'Ratul Sorkar', designation: 'Backend Developer' },
+    { name: 'Rahim Uddin', designation: 'Web Developer' },
+    { name: 'Rabondranath T', designation: 'Graphics Designer' },
+    { name: 'Debobroto Babu', designation: 'Programer' },
+    { name: 'Susanto Pal', designation: 'Software Developer' },
   ]
-  const employesRoll = [
-    '1250',
-    '1251',
-    '1252',
-    '1253',
-    '1254',
-    '1255',
-  ]
+
+  // const employesName = [
+  //   'Rahim Uddin',
+  //   'Nabila',
+  //   'Emon',
+  //   'Nobita',
+  //   'Sohag',
+  //   'Arafat',
+  // ]
+  // const employesRoll = [
+  //   '1250',
+  //   '1251',
+  //   '1252',
+  //   '1253',
+  //   '1254',
+  //   '1255',
+  // ]
   return (
     <div className="App">
-      <Project name={employesName[0]} roll={employesRoll[0]}></Project>
-      <Project name={employesName[1]} roll={employesRoll[1]}></Project>
-      <Project name={employesName[2]} roll={employesRoll[2]}></Project>
-      <Project name={employesName[3]} roll={employesRoll[3]}></Project>
-      <Project name={employesName[4]} roll={employesRoll[4]}></Project>
-      <Project name={employesName[5]} roll={employesRoll[5]}></Project>
+      {employesData.map(employ => <Project name={employ.name} designation={employ.designation}></Project>)}
+
+      {/* <Project name={employesName[0]} roll={employesRoll[0]}></Project> */}
     </div>
   );
 }
@@ -41,7 +47,7 @@ function Project(props) {
   return (
     <div className='Box'>
       <h3>Name: {props.name}</h3>
-      <h5>Roll: {props.roll}</h5>
+      <h5>Designation: {props.designation}</h5>
     </div>
   );
 }
