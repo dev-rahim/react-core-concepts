188e0e81354b3305b487e75a5d7a0f8d91baea47
dev-rahim <rahimuddin5915@gmail.com>
2022-10-11 23:05:32 +0600

    add component data with the use of an array

--- src/App.js
+++ src/App.js
@@ -2,29 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const employesName = [
+    'Rahim Uddin',
+    'Nabila',
+    'Emon',
+    'Nobita',
+    'Sohag',
+    'Arafat',
+  ]
+  const employesRoll = [
+    '1250',
+    '1251',
+    '1252',
+    '1253',
+    '1254',
+    '1255',
+  ]
   return (
     <div className="App">
-      <Project name='Rahim Uddin' roll="127219"></Project>
-      <Project name='Nabila' roll="127220"></Project>
-      <Project name='Saiful' roll="127231"></Project>
-      <Project name='Rabiha' roll="127232"></Project>
-      <Project name='Soriful' roll="127233"></Project>
-      <Project name='Sakibul' roll="127234"></Project>
+      <Project name={employesName[0]} roll={employesRoll[0]}></Project>
+      <Project name={employesName[1]} roll={employesRoll[1]}></Project>
+      <Project name={employesName[2]} roll={employesRoll[2]}></Project>
+      <Project name={employesName[3]} roll={employesRoll[3]}></Project>
+      <Project name={employesName[4]} roll={employesRoll[4]}></Project>
+      <Project name={employesName[5]} roll={employesRoll[5]}></Project>
     </div>
   );
 }
 
 function Project(props) {
-  const as = {
-    color: 'red',
-    background: 'aqua',
-    margin: '10px 20px',
-    borderRadius: '10px',
-    border: '3px solid green'
-  }
+  /*  const box = {
+     color: 'red',
+     background: 'aqua',
+     margin: '10px 20px',
+     borderRadius: '10px',
+     border: '3px solid green'
+   } */
   return (
-    <div style={as}>
-      <h3>Roll: {props.name}</h3>
+    <div className='Box'>
+      <h3>Name: {props.name}</h3>
       <h5>Roll: {props.roll}</h5>
     </div>
   );
