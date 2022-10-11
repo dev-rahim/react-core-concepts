9eac75d70da897de0f731c35d77b2dd53d1d9661
dev-rahim <rahimuddin5915@gmail.com>
2022-10-11 22:38:00 +0600

    updating components data manually

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const container = {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Project name='Rahim Uddin' roll="127219"></Project>
+      <Project name='Kabila' roll="127220"></Project>
+      <Project name='Saiful' roll="127231"></Project>
+      <Project name='Sabiha' roll="127232"></Project>
+      <Project name='Soriful' roll="127233"></Project>
+      <Project name='Sakibul' roll="127234"></Project>
     </div>
   );
 }
+
+const Project = (props) => (
+  <div className="Box">
+    <h3>Roll: {props.name}</h3>
+    <h5>Roll: {props.roll}</h5>
+  </div>
+);
 
 export default App;
